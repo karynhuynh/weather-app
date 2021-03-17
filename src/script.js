1248d45e5c499915849cc8f8ca604e700071ad74
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-03-16 21:04:11 -0700

    Included the units (f and c) conversion

--- src/script.js
+++ src/script.js
@@ -1,94 +1,147 @@
 // Display date and time
 
-const currentTime = new Date()
+const currentTime = new Date();
 
 function dateTime() {
-  let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  let day = days[currentTime.getDay()]
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[currentTime.getDay()];
 
-  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-  let month = months[currentTime.getMonth()]
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[currentTime.getMonth()];
 
-  let date = currentTime.getDate()
-  let year = currentTime.getFullYear()
+  let date = currentTime.getDate();
 
-  let hour = currentTime.getHours()
-  if(hour < 10) {
-    hour = `0${hour}`
+  let year = currentTime.getFullYear();
+
+  let hour = currentTime.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
   }
 
-  let minutes = currentTime.getMinutes()
-  if(minutes < 10) {
-    minutes = `0${minutes}`
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
 
-  let heading = document.querySelector('h6')
-  heading.innerHTML = `${day}, ${month} ${date}, ${year} on ${hour}:${minutes}`
+  let dateTimeSentence = document.querySelector("h6");
+  dateTimeSentence.innerHTML = `${day}, ${month} ${date}, ${year} on ${hour}:${minutes}`;
 }
 
-dateTime()
+dateTime();
 
-// Display city and temperature of city
-
-const apiKey = '563b8c646e928f0609edc6757e3848c7'
-const apiEndpoint = 'https://api.openweathermap.org/data/2.5/weather?'
-let units = 'metric'
-
-function displayCity(event) {
-  event.preventDefault()
-  let citySearch = document.querySelector('#city-input')
-  let searchedCity = citySearch.value
-  let cityApiUrl = `${apiEndpoint}q=${searchedCity}&appid=${apiKey}&units=${units}`
-
-  axios.get(cityApiUrl).then(getTemperature)
-}
-
-//display city search input
-
-let cityInput = document.querySelector('#input-form')
-cityInput.addEventListener('submit', displayCity)
-
-//temperature of city
-
-function getTemperature(response) {
-  let city = response.data.name
-  let cityHeading = document.querySelector('h3')
-  cityHeading.innerHTML = `${city}`
-
-  let temperature = Math.round(response.data.main.temp)
-  let displayTemperature = document.querySelector('.temperature')
-  displayTemperature.innerHTML = `${temperature}`
-
-  let weatherDescription = response.data.weather[0].main
-  let displayWeather = document.querySelector('.weather-description')
-  displayWeather.innerHTML = `${weatherDescription}`
-
-  let humidityPercentage = response.data.main.humidity
-  let displayHumidity = document.querySelector('.humidity-percentage')
-  displayHumidity.innerHTML = `${humidityPercentage}`
-
-  let windSpeed = response.data.wind.speed
-  let displayWind = document.querySelector('.wind-speed')
-  displayWind.innerHTML = `${windSpeed}`
-}
-
+// Display city and temperature
 // current location
 
+const apiKey = "563b8c646e928f0609edc6757e3848c7";
+const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+let units = "metric";
+
 function getCoordinates(position) {
-  let latitude = Math.round(position.coords.latitude)
-  let longitude = Math.round(position.coords.longitude)
-  let coordinatesUrl = `${apiEndpoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let coordinatesUrl = `${apiEndpoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
 
-  axios.get(coordinatesUrl).then(getTemperature)
+  axios.get(coordinatesUrl).then(getTemperature);
 }
-
-let currentLocation = document.querySelector('#current-location-button')
-currentLocation.addEventListener('click', getNavigator)
 
 function getNavigator(event) {
-  event.preventDefault()
-  navigator.geolocation.getCurrentPosition(getCoordinates)
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getCoordinates);
 }
 
+let currentLocation = document.querySelector("#current-location-button");
+currentLocation.addEventListener("click", getNavigator);
 
+// Search for the city
 
+function searchInput(event) {
+  event.preventDefault();
+  let citySearch = document.querySelector("#city-input");
+  let searchedCity = citySearch.value;
+  let apiUrl = `${apiEndpoint}q=${searchedCity}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(getTemperature);
+}
+
+let cityInput = document.querySelector("#input-form");
+cityInput.addEventListener("submit", searchInput);
+
+// Temperature information
+
+function getTemperature(response) {
+  let city = response.data.name;
+  let cityName = document.querySelector("h2");
+  cityName.innerHTML = `${city}`;
+
+  let temperature = Math.round(celciusTemperature);
+  let mainDegree = document.querySelector(".temperature");
+  mainDegree.innerHTML = `${temperature}`;
+
+  let weather = response.data.weather[0].description;
+  let weatherDescription = document.querySelector(".weather-description");
+  weatherDescription.innerHTML = `${weather}`;
+
+  let humidity = response.data.main.humidity;
+  let humidityPercentage = document.querySelector(".humidity-percentage");
+  humidityPercentage.innerHTML = `${humidity}`;
+
+  let wind = response.data.wind.speed;
+  let windSpeed = document.querySelector(".wind-speed");
+  windSpeed.innerHTML = `${wind}`;
+
+  celciusTemperature = response.data.main.temp;
+}
+
+//Convert celsius to fahrenheit
+
+let celciusTemperature = null;
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let fTemperature = Math.round(celciusTemperature * (9 / 5) + 32);
+  let displayTemperature = document.querySelector(".temperature");
+  // remove the active class from the celsius link:
+  celsiusLink.classList.remove("active");
+  // add the active class to the fahrenheit link:
+  fahrenheitLink.classList.add("active");
+  displayTemperature.innerHTML = fTemperature;
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+// Convert fahrenheit to celsius
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let displayTemperature = document.querySelector(".temperature");
+  // add the active class from the celsius link:
+  celsiusLink.classList.add("active");
+  // remove the active class to the fahrenheit link:
+  fahrenheitLink.classList.remove("active");
+  displayTemperature.innerHTML = Math.round(celciusTemperature);
+}
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", convertToCelsius);
